419b71f705065c2f4e71cc2372624cd7df4203e7
Enzo Pinotti <enzopinottii@gmail.com>
2024-03-29 18:44:45 -0300

    Segunda versión4

--- src/components/Logo.js
+++ src/components/Logo.js
@@ -4,7 +4,7 @@ import { NavLink } from 'react-router-dom'
 const Logo = () => {
   return (
     <article className='logo-container'>
-        <NavLink to='/'><img src='/img/logo.png' alt='logo' className="logo-img" /></NavLink>
+        <NavLink to='/'><img src='./img/Logo.png' alt='logo' className="logo-img" /></NavLink>
     </article>
   )
 }
